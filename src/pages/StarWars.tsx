c9d191efef47f7ba5ef67214fd6dbdd9d10f9b2e
LymarMaryna <marina.web.php@gmail.com>
2024-06-19 16:00:33 +0300

    feat: pagination

--- src/pages/StarWars.tsx
+++ src/pages/StarWars.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { CardList } from '../components';
 import { Character } from '../utils';
 import { getCharacters } from '../api';
+
+const MAX_PAGE_COUNT = 9; // TODO: we need synchronies this with API response
 
 /**
  * StarWars explorer
@@ -14,16 +16,36 @@ const StarWarsView = () => {
   useEffect(() => {
     async function getData() {
       const dataFromApi = await getCharacters(pageNumber, searchText);
-      console.log('getData()', dataFromApi);
+      console.log('getData()', pageNumber, dataFromApi);
       setCharacters(dataFromApi);
     }
     getData();
   }, [pageNumber, searchText]);
 
+  const onPrevClick = useCallback(() => setPageNumber((old) => Math.max(old - 1, 1)), []);
+
+  const onNextClick = () => setPageNumber((old) => Math.min(old + 1, MAX_PAGE_COUNT)); // TODO: maybe we need useCallback here, but MAX_PAGE_COUNT could be a state
+
+  const prevButtonDisabled = pageNumber < 2;
+
+  const nextButtonDisabled = pageNumber >= MAX_PAGE_COUNT;
+
   return (
     <div>
-      <button onClick={() => setPageNumber(pageNumber + 1)}>next</button>
-      <button onClick={() => setSearchText('dart')}>Search Dart</button>
+      <button disabled={prevButtonDisabled} onClick={onPrevClick}>
+        prev
+      </button>
+      <button disabled={nextButtonDisabled} onClick={onNextClick}>
+        next
+      </button>
+      <button
+        onClick={() => {
+          setSearchText('dart');
+          setPageNumber(1);
+        }}
+      >
+        Search Dart
+      </button>
       <CardList characters={characters} />
     </div>
   );
